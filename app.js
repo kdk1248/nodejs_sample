329bb65f47da4bf1185b2b6693d6967048a7410e
kong <alice3976@g.hongik.ac.kr>
2025-01-08 12:40:02 +0900

    타입스크립트 파일 생성 및 컴파일  명시적 타입 선언 방법

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-console.log("hello")
-
-let a = 10; //숫자, number
-let b = "Hello"; // 문자열, string
-let c = true;// false, Boolean 참,거짓
-let d = [1,2,3,4]; // 배열, array
-let e = { name: "Tom", age: 35} // 객체, Object
+console.log("hello");
+var a = 10; //숫자, number
+var b = "Hello"; // 문자열, string
+var c = true; // false, Boolean 참,거짓
+var d = [1, 2, 3, 4]; // 배열, array
+var e = { name: "Tom", age: 35 }; // 객체, Object
+a = 50;
+b = "50";
 //..
-
-b = 50;
-
-//..
-
-console.log("a=" + a, "type of" + typeof a)
-console.log("b=" + b, "type of" + typeof b)
-console.log("c=" + c, "type of" + typeof c)
-console.log("d[0]=" + d[0], "type of" + typeof d)
-console.log("e.name=" + e.name, "e.age=" + e.age, "type of" + typeof e)
-
+console.log("a=" + a, "type of" + typeof a);
+console.log("b=" + b, "type of" + typeof b);
+console.log("c=" + c, "type of" + typeof c);
+console.log("d[0]=" + d[0], "type of" + typeof d);
+console.log("e.name=" + e.name, "e.age=" + e.age, "type of" + typeof e);
 //예시
-
-let columnLength = 10; //길이를 cm단위,  number로 지정
+var columnLength = 10; //길이를 cm단위,  number로 지정
 //..개발 진행 과정
 //..개발 진행 과정
 //..개발 진행 과정
@@ -30,18 +24,16 @@ let columnLength = 10; //길이를 cm단위,  number로 지정
 //..개발 진행 과정
 //..개발 진행 과정
 //..개발 진행 과정
-columnLength = "30cm";
-
+columnLength = 30;
 //..개발 진행 과정
 //..개발 진행 과정
 //..개발 진행 과정
 //..개발 진행 과정
 //..개발 진행 과정
-let addLength = columnLength + 5; // 15cm.. 35cm 처럼 나오겠지
-let meterLength = addLength / 10;
-
+var addLength = columnLength + 5; // 15cm.. 35cm 처럼 나오겠지
+var meterLength = addLength / 10;
 //..개발 진행 과정
 //..개발 진행 과정
 //..개발 완료 
-console.log(addLength)
-console.log(meterLength)
+console.log(addLength);
+console.log(meterLength);
