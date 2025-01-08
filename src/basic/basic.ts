ebe18692e2da8cba2190fd6a7156ee72e60bd5cd
kong <alice3976@g.hongik.ac.kr>
2025-01-08 16:18:36 +0900

    타입 지정 방시, 명시적 타입 지정을 권장함

--- src/basic/basic.ts
+++ src/basic/basic.ts
@@ -6,6 +6,7 @@ message.toLowerCase();
 // message();
 
 
+
 const announcement = "Hello World!;"
 // Howquickly can you sopt the typos
 announcement
@@ -22,3 +23,38 @@ function flipCoin(){
     //Meant to be Math.random()
     return Math.random() < 0.5; 
 }
+
+
+//1. 추론저 타입 지정
+let x = 3;
+// x = [0,1,null]
+
+class Animal {}
+class Rhino extends Animal {
+    hasHorn: boolean = true;
+}
+
+class Elephant extends Animal {
+    hasTrunk: boolean = true;
+}
+
+class Snake extends Animal {
+    hasLegs: boolean = true;
+}
+
+let zoo = [new Rhino(), new Elephant(), new Snake()];
+//
+// zoo는 어떻게 구별되어야 하는가?
+// Animal [] <- Animal 배열로 추론 될 것이다 그러나 각 객체의 배열로 나타남 즉 다른 타입이 지정됨
+// 사실은 let zoo:Animal[] = [new Rhino(), new Elephant(), new Snake()];로 나오길 원함
+
+//2. 명시적 타입 지정 (Explict Types, 중요하다/ ts를 사용하는 이유이기도 하다)
+
+//This is an ndustrial-grade general-purpose greeter function:
+function greet(person:string, date:Date){
+    console.log(`Hello ${person}, today is ${date}!`);
+}
+
+greet("Brendan",new Date());
+
+
