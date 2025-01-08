df6379d12a33792dee15c39e97412d3a60069c43
kong <alice3976@g.hongik.ac.kr>
2025-01-08 15:37:10 +0900

    Greeter 타입 스크립트 소개

---
+++ src/greeter.js
@@ -0,0 +1,6 @@
+function greeter(person){
+    return "Hello," + person;
+}
+var user ="kdk";
+console.log(greeter(user));
+//documnet.body.textContent = greeter(user);
---
+++ src/greeter.ts
@@ -0,0 +1,5 @@
+function greeter(person:string){
+    return "Hello," + person;
+}
+var user ="kdk";
+console.log(greeter(user));
