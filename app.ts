0fd69b929b09476e3cd247ea1d66c62d916bfc5f
kong <alice3976@g.hongik.ac.kr>
2025-01-08 12:49:19 +0900

    변수와 메서드의 타입 명시 방법

--- app.ts
+++ app.ts
@@ -49,3 +49,10 @@ let meterLength = addLength / 10;
 //..개발 완료 
 console.log(addLength)
 console.log(meterLength)
+
+
+//-------
+//메서드 타입 명시 방법 
+function add(x:number,y:number):number{
+    return x+y;
+}
